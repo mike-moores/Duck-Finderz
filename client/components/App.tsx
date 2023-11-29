edec5c1213d445821aedab53126b21127e708d94
Sam McPhail <sam.mcphail@hotmail.co.nz>
2023-11-29 16:04:56 +1300

    First commit

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,86 @@
+import {
+  Box,
+  Card,
+  CardBody,
+  Center,
+  Container,
+  Grid,
+  GridItem,
+  Heading,
+  Text,
+  Image,
+  Stack,
+} from '@chakra-ui/react'
+
 function App() {
   return (
     <>
       <header className="header">
-        <h1>My Collection</h1>
+        <Container bgColor="grey">
+          <Grid templateColumns="repeat(3, 1fr)">
+            <GridItem colSpan={3}>
+              {' '}
+              <Center>
+                <Heading size="3xl">Duck Collection!!</Heading>
+              </Center>
+            </GridItem>
+            <GridItem colSpan={2}>
+              {' '}
+              <Text fontSize="4xl">New Duck</Text>
+            </GridItem>
+            <GridItem colSpan={1}>
+              {' '}
+              <Text fontSize="4xl">Login</Text>
+            </GridItem>
+            <GridItem colSpan={3}>
+              <Box bg="tomato" w="100%" p={4} color="white">
+                Common
+              </Box>
+            </GridItem>
+
+            <GridItem colSpan={1}>
+              <Grid templateColumns="repeat(3, 1fr)">
+                <Card>
+                  <CardBody>
+                    <Image src="/images/pixel-rubber-duck-bubbles.png"></Image>
+                  </CardBody>
+                </Card>
+                <Text>x9</Text>
+              </Grid>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <Grid templateColumns="repeat(3, 1fr)">
+                <Card>
+                  <CardBody>
+                    <Image src="/images/pixel-rubber-duck-bubbles.png"></Image>
+                  </CardBody>
+                </Card>
+                <Text>x9</Text>
+              </Grid>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <Grid templateColumns="repeat(3, 1fr)">
+                <Card>
+                  <CardBody>
+                    <Image src="/images/pixel-rubber-duck-bubbles.png"></Image>
+                  </CardBody>
+                </Card>
+                <Text>x9</Text>
+              </Grid>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <Grid templateColumns="repeat(3, 1fr)">
+                <Card>
+                  <CardBody>
+                    <Image src="/images/pixel-rubber-duck-bubbles.png"></Image>
+                  </CardBody>
+                </Card>
+                <Text>x9</Text>
+              </Grid>
+            </GridItem>
+          </Grid>
+          <Text></Text>
+        </Container>
       </header>
       <section className="main">{/* add your code here */}</section>
     </>
--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import App from './components/App.tsx'
 
@@ -8,9 +9,11 @@ const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
   )
 })
