908497846f8f67ffdc3087a004c08904941c8c76
Sam McPhail <sam.mcphail@hotmail.co.nz>
2023-12-06 15:57:11 +1300

    Adds final styling for deployment

--- client/components/Ducks.tsx
+++ client/components/Ducks.tsx
@@ -1,5 +1,14 @@
 import { Link } from 'react-router-dom'
-import { Card, Image, Grid, GridItem, Text } from '@chakra-ui/react'
+import {
+  Card,
+  Image,
+  Grid,
+  GridItem,
+  Text,
+  Center,
+  Heading,
+  Container,
+} from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
 import { Duck } from '../../models/Ducks'
 import { getDuck } from '../apis/api'
@@ -12,43 +21,53 @@ export default function Ducks() {
   } = useQuery<Duck[]>({ queryKey: ['Ducks'], queryFn: () => getDuck() })
 
   if (!ducks || isLoading) {
-    return <p>Loading...</p>
+    return (
+      <Center>
+        <Heading fontFamily="shadows into  light">Loading...</Heading>
+      </Center>
+    )
   }
 
   if (error) {
-    return <p>Error fetching Ducks: </p>
+    return (
+      <Center>
+        <Heading fontFamily="shadows into  light">An error occurred...</Heading>
+      </Center>
+    )
   }
   return (
     <>
-      <Grid
-        templateColumns="repeat(5, 1fr)"
-        height="100vh"
-        margin="0 100px 0 100px"
-      >
-        {ducks.map((d) => (
-          <GridItem
-            alignContent="center"
-            margin="10px auto 10px auto"
-            key={d.id}
-          >
-            <Text fontWeight="bold" textAlign="center">
-              {d.name}
-            </Text>
-            <Link to={`/ducks/${d.id}`}>
-              <Card borderColor="#044c34" borderWidth="1px">
-                {/* <CardBody> */}
-                <Image
-                  src={d.image}
-                  alt={d.name}
-                  width="150px"
-                  height="150px"
-                ></Image>
-                {/* </CardBody> */}
-              </Card>
-            </Link>
-          </GridItem>
-        ))}
-      </Grid>
+      <Container minH="100vh" minW={{ base: '50vh', lg: '100vh' }}>
+        <Grid
+          templateColumns={{ base: 'repeat(3, 1fr)', lg: 'repeat(5, 1fr)' }}
+        >
+          {ducks.map((d) => (
+            <GridItem
+              alignContent="center"
+              margin="20px 20px 20px 20px"
+              key={d.id}
+              width="130px"
+              height="130px"
+            >
+              <Text fontWeight="bold" textAlign="center">
+                {d.name}
+              </Text>
+              <Link to={`/ducks/${d.id}`}>
+                <Card borderColor="#044c34" borderWidth="1px">
+                  {/* <CardBody> */}
+                  <Image
+                    src={d.image}
+                    alt={d.name}
+                    width="100%"
+                    height="100%"
+                  ></Image>
+                  {/* </CardBody> */}
+                </Card>
+              </Link>
+            </GridItem>
+          ))}
+        </Grid>
+      </Container>
     </>
   )
 }
